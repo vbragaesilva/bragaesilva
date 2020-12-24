c8ac9312312cf6e624485f0a582736e683c47c90
eFrostByte <nickblack141499@gmail.com>
2020-12-24 18:16:24 +0000

    mudanças no /jogos

--- pages/jogos.js
+++ pages/jogos.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import {DefaultHeader} from '../components/braga.js';
-import {ListItem} from '../components/braga.js'
+import {DefaultHeader, ListItem} from '../components/braga.js';
 function Jogos(){
 	
 	return(
@@ -37,30 +36,5 @@ function Jogos(){
 		</div>
 	)
 }
-
-/* function Listao(props){
-    const title = props.title
-    const img_src = props.img 
-    const scr = props.href
-    const description = props.children
-    return(
-        <div>
-    
-            <div title={title} className="game-wrap">
-                <Link href={scr}>
-                    <a className="ia">
-                        <img src={img_src} alt={title} className="thumb"/>
-                        <h3 className="ah3">
-                            {title}
-                        </h3>
-                    </a>
-                </Link>
-                <div className="divp">
-                    <p className="desc">{description}</p>
-                </div>
-            </div>
-        </div>
-    )
-} */
 
 export default Jogos
