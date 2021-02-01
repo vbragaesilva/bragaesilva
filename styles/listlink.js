1b002c679761d4c6f2d80a820ad9ec313fbe5897
vbragaesilva <vbragaesilva@users.noreply.github.com>
2021-02-01 09:50:33 -0300

    estilos de componente

--- styles/listlink.js
+++ styles/listlink.js
@@ -1,7 +1,30 @@
 import css from 'styled-jsx/css'
 
 export default css.global`
-    .ah3{
-        background-color:red;
-    }
+.ia{
+    
+    display: inline-block;
+    padding: 54px 1%;
+    margin: 20px;
+    text-align: center;
+}
+
+.item-wrap{
+    width: 100%;
+    height:247px;
+    position: relative;
+}
+
+.ah3{
+    text-decoration: underline;
+    text-decoration-color: var(--cinza-escuro);
+    text-decoration-thickness: 1px;
+    font-weight: 500;
+    order:2;
+}
+
+.ah3.noimage{
+    margin-top: 28px;
+
+}
 `
